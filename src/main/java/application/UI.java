3bd37365b15091bda33099d42b8f66a190625858
Phyllipe <phylipe.sjc@hotmail.com>
2023-10-19 19:10:25 -0300

    checkmate logic

--- src/main/java/application/UI.java
+++ src/main/java/application/UI.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class UI {
   public static final String ANSI_RESET = "\u001B[0m";
@@ -54,10 +53,15 @@ public class UI {
     printCapturedPieces(captured);
     System.out.println();
     System.out.println("Turn: " + chessMatch.getTurn());
-    System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 
-    if (chessMatch.getCheck()) {
-      System.out.println("CHECK!");
+    if (!chessMatch.getCheckMate()) {
+      System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+      if (chessMatch.getCheck()) {
+        System.out.println("CHECK!");
+      }
+    } else {
+      System.out.println("CHECKMATE!");
+      System.out.println("Winner: " + chessMatch.getCurrentPlayer());
     }
   }
 
